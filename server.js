464a266e237f09537f07c9975b2cbfc43cf3b04c
Pat Palmerston <patpalmerston@gmail.com>
2019-09-04 11:54:55 -0700

    server and routes up with middleward packages

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
-const express = 'express';
+const express = require('express');
+const configureMiddleware = require('././data/middleware/middleware');
+const postsRouter = require('./posts/postRouter');
+const usersRouter = require('./users/userRouter');
 
 const server = express();
 
+configureMiddleware(server);
+
+server.use('/api/posts', postsRouter);
+server.use('/api/users', usersRouter);
+
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+	res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {
-
-};
+function logger(req, res, next) {}
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,6 +1,8 @@
-const express = 'express';
+const express = require('express');
 
 const router = express.Router();
+
+
 
 router.post('/', (req, res) => {
 
